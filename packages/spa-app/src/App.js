8611c32ff3a0d0107a0276eeb99d0a77396a06fb
Kurtis Kemple <kurtiskemple@gmail.com>
2018-05-02 11:47:43 -0400

    add: add create-react-app-example

--- packages/spa-app/src/App.js
+++ packages/spa-app/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from 'uc-example-react-eu';
+
+const title = 'Example Card';
+const subtitle = 'Rendered using react-primitives';
+const image = { uri: 'https://source.unsplash.com/random' };
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card title={title} subtitle={subtitle} image={image} />
       </div>
     );
   }
