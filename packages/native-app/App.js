a9f3691ffc23ba3a9ce86b4ec713fd493be28801
Kurtis Kemple <kurtiskemple@gmail.com>
2018-05-15 14:12:24 +0200

    add: header to native example

--- packages/native-app/App.js
+++ packages/native-app/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Card } from 'uc-example-react-eu';
+import { Header, Card } from 'uc-example-react-eu';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Header
+          subtitle="It's great to see all of you"
+          title="Hello, React Europe"
+          style={{ marginBottom: 8 }}
+        />
         <Card
           image={{ uri: 'https://source.unsplash.com/random' }}
           subtitle="Rendered using react-primitives"
