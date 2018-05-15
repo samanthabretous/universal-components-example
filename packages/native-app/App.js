1e32011981da444c63e8d7a61a9525ecb18b10f7
Kurtis Kemple <kurtiskemple@gmail.com>
2018-05-15 14:31:34 +0200

    add: header to spa example

--- packages/native-app/App.js
+++ packages/native-app/App.js
@@ -15,7 +15,6 @@ export default class App extends React.Component {
           image={{ uri: 'https://source.unsplash.com/random' }}
           subtitle="Rendered using react-primitives"
           title="Example Card"
-          style={{ borderColor: '#eeeeee', borderWidth: 1 }}
         />
       </View>
     );
--- packages/spa-app/src/App.js
+++ packages/spa-app/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import { Card } from 'uc-example-react-eu';
+import { Header, Card } from 'uc-example-react-eu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header
+          subtitle="It's great to see all of you"
+          title="Hello, React Europe"
+          style={{ marginBottom: 8 }}
+        />
         <Card
           image={{ uri: 'https://source.unsplash.com/random' }}
           subtitle="Rendered using react-primitives"
           title="Example Card"
-          style={{ borderColor: '#eeeeee', borderWidth: 1 }}
+          style={{ margin: 16 }}
         />
       </div>
     );
