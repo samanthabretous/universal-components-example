07d5bbffe761946b64aedd3eabd200f091cd63b5
Kurtis Kemple <kurtiskemple@gmail.com>
2018-05-15 14:09:12 +0200

    fix: no data card and storybook examples

--- packages/universal-components/src/card/__stories__/index.stories.js
+++ packages/universal-components/src/card/__stories__/index.stories.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Card from '../index';
 
-storiesOf('Card', module).add('Example', () => (
-  <div style={{ padding: 16 }}>
+storiesOf('Card', module)
+  .add('No Data', () => (
+    <div style={{ padding: 16 }}>
+      <Card />
+    </div>
+  ))
+  .add('With Data', () => (
+    <div style={{ padding: 16 }}>
+      <Card
+        image={{ uri: 'https://source.unsplash.com/random' }}
+        subtitle="Rendered using react-primitives"
+        title="Example Card"
+      />
+    </div>
+  ))
+  .add('With Custom Styles', () => (
     <Card
       image={{ uri: 'https://source.unsplash.com/random' }}
       subtitle="Rendered using react-primitives"
       title="Example Card"
-      style={{ borderColor: '#eeeeee', borderWidth: 1 }}
+      style={{ margin: 16 }}
     />
-  </div>
-));
+  ));
