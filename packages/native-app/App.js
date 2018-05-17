69171b9bee45f3a6d463fefed3b946e63359c0fb
Kurtis Kemple <kurtiskemple@gmail.com>
2018-05-17 17:24:00 +0200

    fix: small adjustments

--- packages/native-app/App.js
+++ packages/native-app/App.js
@@ -6,11 +6,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Header
-          subtitle="It's great to see all of you"
-          title="Hello, React Europe"
-          style={{ marginBottom: 8 }}
-        />
         <Card
           image={{ uri: 'https://source.unsplash.com/random' }}
           subtitle="Rendered using react-primitives"
--- packages/spa-app/src/App.js
+++ packages/spa-app/src/App.js
@@ -5,11 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header
-          subtitle="It's great to see all of you"
-          title="Hello, React Europe"
-          style={{ marginBottom: 8 }}
-        />
         <Card
           image={{ uri: 'https://source.unsplash.com/random' }}
           subtitle="Rendered using react-primitives"
